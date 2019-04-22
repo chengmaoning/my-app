0b58ad239ae800bccc9b81b72de3d0fede88c0c9
chengmaoning <chengmaoning@sina.com>
2019-04-22 19:55:25 +0800

    format

--- src/component/TemperatureInput.js
+++ src/component/TemperatureInput.js
@@ -1,31 +1,30 @@
-import React from 'react';
-
+import React from "react";
 
 const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
-  };
-class TemperatureInput extends React.Component{
-    constructor(props){
-        super(props);
+  c: "Celsius",
+  f: "Fahrenheit"
+};
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(e){
-        this.props.onTemperatureChange(e.target.value);
-    }
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
 
-    render(){
-        const temperature = this.props.temperature;
-        const scale = this.props.scale;
-        return (
-            <fieldset>
-                <legend>Enter temperature in {scaleNames[scale]}:</legend>
-                <input value={temperature} onChange={this.handleChange}/>
-            </fieldset>
-        );
-    }
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
 }
 
 export default TemperatureInput;
